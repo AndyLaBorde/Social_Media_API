45f3fabb6652829d2752ec797780f8630bd3378d
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2023-01-05 16:59:21 -0700

    created reactionSchema

--- models/thought.js
+++ models/thought.js
@@ -2,6 +2,42 @@ const { Schema, model, Types } = require('mongoose');
 // import momnet to format timestamp
 const moment = require('moment');
 
+// reaction schema
+const reactionSchema = new Schema (
+    {
+        // reactionId , mongoose ObjectId data type, default value is set to new ObjectId
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        //reactionBody, STRING REQUIRED MAX=280
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        // username STRING REQUIRED
+        username: {
+            type: String,
+            required: true,
+        },
+        // createdAt DATE, set default value to the current timestamp, 
+        // use a getter method to format the timestamp on query
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM Do, YYYY [at] hh:mm a")
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
 
 // thought schema
 const thoughtSchema = new Schema (
@@ -14,11 +50,11 @@ const thoughtSchema = new Schema (
             maxlength: 280,
         },
         // CREATED AT TIMESTAMP DATE, SET DEFAULT VALUE TO CURRENT TIMESTAMP,
-        // USE A GETTER METHOD TO FORMAT THE TIMESTAMP
+        // USE A GETTER METHOD & MOMENT%TO FORMAT THE TIMESTAMP
         createdAt: {
             type: Date,
             default: Date.now,
-            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+            get: createdAtVal => moment(createdAtVal).format("MMM, Do, YYYY [at] hh:mm a"),
         },
         // USERNAME MUST BE STRING, REQUIRED
         username: {
@@ -35,7 +71,8 @@ const thoughtSchema = new Schema (
         },
         id: false,
     }
-)
+);
+
 // create a virtual called reactionCount that retrieves 
 // the length of the thought's reactions array field query
 thoughtSchema.virtual('reactionCount')
